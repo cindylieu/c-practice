0024d9081ea4db6cd6881674b8baa9c2a768d995
Cindy Lieu <lieuzcindy@gmail.com>
2020-08-02 16:14:45 -0400

    added longest common subsequence homework

---
+++ longestCommonSubsequence/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int longestCommonSubsequence(char * text1, char * text2);
+int lcs(char *text1, char *text2, int len1, int len2);
+int max (int a, int b);
+
+int main() {
+
+    printf("%d\n", longestCommonSubsequence("abcde", "ace"));
+
+    printf("%d", longestCommonSubsequence("abc", "ace"));
+
+    printf("%d", longestCommonSubsequence("dgf", "ace"));
+
+    return 0;
+}
+
+int longestCommonSubsequence(char * text1, char * text2){
+
+    int len1 = strlen(text1);
+    int len2 = strlen(text2);
+
+    if (text1 == NULL || text2 == NULL || len1 == 0 || len2 == 0) {
+        return 0;
+    }
+
+    int dp[len1+1][len2+1];
+    int i, j;
+
+    for (i = 0; i < len1+1; i++) {
+        for (j = 0; j < len2+1; j++ ) {
+            if (i == 0||j == 0) {
+                dp[i][j] = 0;
+            } else if (text1[i-1] != text2[j-1]) {
+                if (dp[i-1][j] >= dp[i][j-1]) {
+                    dp[i][j] = dp[i-1][j];
+                } else
+                    dp[i][j] = dp[i][j-1];
+            } else {
+                dp[i][j] = dp[i-1][j-1] + 1;
+            }
+        }
+    }
+    return dp[len1][len2];
+}
